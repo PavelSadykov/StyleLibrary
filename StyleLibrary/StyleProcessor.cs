59fb43870a9d45ab7141eef9fa380b908942dba8
PavelSadykov <2439747@gmail.com>
2023-12-16 23:48:48 +0500

    Добавьте файлы проекта.

---
+++ StyleLibrary/StyleProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace StyleLibrary
+{
+    public class StyleProcessor
+    {
+        public static void ChangeButtonStyle(Button button, Color backColor, Color foreColor, Font font)
+        {
+            button.BackColor = backColor;
+            button.ForeColor = foreColor;
+            button.Font = font;
+        }
+    }
+}
